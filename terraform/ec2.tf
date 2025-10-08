5bd854f73dc197876b79b94983b4836a15b462ca
mdeadwiler <mdeadwiler85@gmail.com>
2025-10-08 15:13:24 -0400

    Add security group for EC2 and update instance config
    Created a new security group resource allowing SSH, HTTPS, and Node.js app traffic for the EC2 instance. Updated the EC2 instance to use the new security group, changed the instance type to t3.micro, and updated tags to reflect a Node.js server.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,10 +8,54 @@
 
 resource "aws_instance" "tf_ec2_instance" {
   ami           = "ami-0360c520857e3138f" # Ubuntu image
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.tf_ec2_sg.id]
   key_name = "terraform-ec2"
+  
   tags = {
-    Name = "HelloWorld"
+    Name = "Nodejs-server"
   }
 }
+
+# Security Group
+resource "aws_security_group" "tf_ec2_sg" {
+  name        = "nodejs-server-sg"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = "vpc-0a55ff24d096fe43d"  # default VPC
+
+  ingress {
+        description = "Allow TLS from VPC"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # allow all IPs
+    }
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # For best practices I would normally use my IP address or a private IP address
+    }
+
+    ingress {
+        description = "TCP"
+        from_port = 3000 # for nodejs app
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+# Keep in mind as said earlier this isn't typcally best practices. This is for education purposes 
+  tags = {
+    Name = "Nodejs-server-sg"
+  }
+}
+
